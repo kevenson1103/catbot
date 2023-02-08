f0e22f362d871bffa21c232c81f514fa5f85a2c2
rellyman53 <nevenson2016@gmail.com>
2023-02-08 09:46:28 -0600

    add data files, add command usage tracker to cat.js

--- dsc/cat.js
+++ dsc/cat.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const Discord = require('discord.js');
 const cats = require('cat-ascii-faces');
 
+const fs = require('fs');
+
 module.exports.run = async (client, message, args) => {
     axios.get(`https://api.thecatapi.com/v1/images/search`)
     .then((cat) => {
@@ -11,6 +13,16 @@ module.exports.run = async (client, message, args) => {
         .setImage(`${cat.data[0].url}`)
         .setTimestamp()
         message.channel.send({embeds:[embed]})
+
+        
+        fs.readFile('./dat/catUsage.txt', 'utf8', (err, data) => {
+            if(err) { console.log(err) }
+
+            let newData = new Uint8Array(Buffer.from(`${+data + 1}`))
+            fs.writeFile('./dat/catUsage.txt', newData, (err) => {
+                if(err) { console.log(err) }
+            })
+        })
     })
     .catch((err) => {
         message.reply(`There was an error getting your cat!`)
