920ed15729b3248ce1cfe74741f6416a56dadf12
rellyman53 <nevenson2016@gmail.com>
2023-02-08 11:01:18 -0600

    add command usage to >stats

--- dsc/stats.js
+++ dsc/stats.js
@@ -1,18 +1,35 @@
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const Discord = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+let fs = require('fs');
 
-module.exports.run = async (client, message, args) => {
-    if(message.author.id !== "312734500880646145") return message.reply('You do not have permission to run this command. Sorry.')
+module.exports.run = async (client, message) => {
 
+// calculating bot uptime
+let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+totalSeconds %= 86400;
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = Math.floor(totalSeconds % 60);
+
+const uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`
+
+// message code
         const embed = new Discord.MessageEmbed()
         .setColor(0xff00d0)
-        .setTitle(`Current Bot Status`)
+        .setTitle(`Stats for CatBot`)
         .setThumbnail(`https://cdn.discordapp.com/attachments/973719071352823838/973720104007237672/279936345_422397323222930_7232341584016774401_n.png`)
-        .setDescription(`
-        Ping: ${Math.round(client.ws.ping)}ms
-        Number of Servers bot is in: ${client.guilds.cache.size}
-        `)
+        .addFields(
+            { name: 'Current Ping:', value: `${Math.round(client.ws.ping)}ms`, inline: true },
+            { name: 'Number of Servers CatBot is in:', value: `${client.guilds.cache.size}`, inline: true },
+            { name: 'Uptime:', value: `${uptime}`, inline: true },
+
+            { name: 'Times >cat has been used:', value: `${fs.readFileSync('./dat/catUsage.txt', { encoding: 'utf8'})}`, inline: true },
+            { name: 'Times >catfact has been used:', value: `${fs.readFileSync('./dat/catFactUsage.txt', { encoding: 'utf8'})}`, inline: true },
+            { name: 'Times >catname has been used:', value: `${fs.readFileSync('./dat/catNamesUsage.txt', { encoding: 'utf8'})}`, inline: true }
+            
+        )
         .setTimestamp()
         message.channel.send({embeds:[embed]})
 }
